6f47639e27407626ed274e06b9092bef0d869ee2
Dan Hackney <dan@haxney.org>
2010-02-06 08:56:10 -0500

    Add index.el.
    Keeps the project list, a list of project names and their repository URL.
    
    Signed-off-by: Dan Hackney <dan@haxney.org>

---
+++ index.el
@@ -0,0 +1,8 @@
+(("anything" "git://github.com/haxney/anything.git")
+ ("anything-config" "git://github.com/haxney/anything-config.git")
+ ("anything-contrib" "git://github.com/haxney/anything-contrib.git")
+ ("bm" "git://github.com/haxney/bm.git")
+ ("package" "git://github.com/haxney/package.git")
+ ("traverselisp" "git://github.com/haxney/traverselisp.git")
+ ("auto-install" "git://github.com/haxney/auto-install.git")
+ )
